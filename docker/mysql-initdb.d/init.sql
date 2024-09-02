77843c995863104f5293b972bbeb096d927e9ca1
Quentin Watt <watt.quentin@gmail.com>
2024-09-02 19:28:21 +0200

    Get tests working with refresh database

---
+++ docker/mysql-initdb.d/init.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE
+IF NOT EXISTS shopper_db_test;
--- shopper-be/tests/TestCase.php
+++ shopper-be/tests/TestCase.php
@@ -4,8 +4,16 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Config;
 
 abstract class TestCase extends BaseTestCase
 {
     use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Config::set('database.connections.mysql.database', 'shopper_db_test');
+    }
 }
