c941d93ca6fe6b1e0ead957bc4ae8499eeb4bcd7
Quentin Watt <watt.quentin@gmail.com>
2024-09-06 00:36:53 +0200

    create a products lsit

--- backoffice/src/components/products/ProductsList.tsx
+++ backoffice/src/components/products/ProductsList.tsx
@@ -2,16 +2,22 @@ import { displayPrice } from "@/helpers/currency";
 import Product from "@/models/Product";
 import ProductsContext from "@/views/Products/ProductsContext";
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
 const ProductsList: React.FC = () => {
   const { products }: { products: Product[] } = useContext(ProductsContext);
 
   return (
-    <ul>
+    <ul className="grid gap-2">
       {products.map((product) => (
-        <li key={product.id} className="border rounded p-3 mb-2 bg-white">
-          <h1 className="font-bold">{product.name}</h1>
-          <div>{displayPrice(product.price_cents)}</div>
+        <li key={product.id}>
+          <Link
+            to={`/products/${product.id}`}
+            className="flex border border-gray-300 rounded p-3 bg-white"
+          >
+            <h3 className="font-bold mr-3">{product.name}</h3>
+            <div>{displayPrice(product.price_cents)}</div>
+          </Link>
         </li>
       ))}
     </ul>
