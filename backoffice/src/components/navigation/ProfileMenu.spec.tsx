15e8ab086c92b0ca2b89331b2914e7834ed52865
Quentin Watt <watt.quentin@gmail.com>
2024-09-12 09:57:34 +0200

    remove unused variable

--- backoffice/src/components/navigation/ProfileMenu.spec.tsx
+++ backoffice/src/components/navigation/ProfileMenu.spec.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import ProfileMenu from "./ProfileMenu";
 import AuthContext from "@/providers/auth/AuthContext";
 
