cea341f621245f09d40ce1ed974b7dcc509957a9
Quentin Watt <watt.quentin@gmail.com>
2024-09-17 12:28:30 +0200

    Fix failing test

--- backoffice/src/components/products/ProductEditForm.spec.tsx
+++ backoffice/src/components/products/ProductEditForm.spec.tsx
@@ -1,7 +1,6 @@
 import ProductEditContext from "@/providers/products/ProductEditContext";
 import { fireEvent, render, screen } from "@testing-library/react";
 import ProductEditForm from "./ProductEditForm";
-import ProductDeleteButton from "./ProductDeleteButton";
 
 vi.mock("./ProductDeleteButton.tsx", () => ({
   default: vi.fn(() => <button>Mock delete button</button>),
